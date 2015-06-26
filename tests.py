9afddf582f1beead08449993b816ff54abb65dbf
Peter Tröger <peter@troeger.eu>
2015-06-26 20:20:38 +0200

    Adding test suite

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+import formats
+
+class DeplyticsTestCase(unittest.TestCase):
+	def setUp(self):
+		self.data=formats.loader(self.fixture)
+
+class FaultTreeTestCase(DeplyticsTestCase):
+	fixture = "fixtures/faulttree.xml"
+
+	def testStringify(self):
+		print(self.data.tree_apply(formats.tree.OP_STRINGIFY))
+
+if __name__ == '__main__':
+    unittest.main()
